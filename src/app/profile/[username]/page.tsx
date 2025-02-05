f9b226fa43d34d0de4ac3fc50923a8606107ef7f
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2025-02-05 23:31:01 +0500

    generateMetadata function for titles & descriptions

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,6 +1,20 @@
+import { getProfileByUsername } from "@/actions/profile.action";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
+
 async function ProfilePage({ params }: { params: { username: string } }) {
-  console.log(params);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <div>ProfilePage</div>;
 }
 
