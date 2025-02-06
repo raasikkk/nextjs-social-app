02a2295e5b078ebe0284a605abf5fc59fa0756c1
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2025-02-06 21:00:50 +0500

    full profile functionality added

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,11 @@
-import { getProfileByUsername } from "@/actions/profile.action";
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
 export async function generateMetadata({
   params,
@@ -14,8 +21,25 @@ export async function generateMetadata({
   };
 }
 
-async function ProfilePage({ params }: { params: { username: string } }) {
-  return <div>ProfilePage</div>;
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
 }
 
-export default ProfilePage;
+export default ProfilePageServer;
